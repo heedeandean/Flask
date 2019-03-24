fbcb8f32a8043bd387c552e7d77a0750feb29dd5
heedeandean <7942ahj@hanmail.net>
2019-03-25 02:43:05 +0900

    flask

--- photolog/photolog/photolog_blueprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from photolog.photolog_logger import Log
-
-photolog = Blueprint('photolog', __name__,
-                     template_folder='../templates',
-                     static_folder='../static')
-
-Log.info('static folder : %s' % photolog.static_folder)
-Log.info('template folder : %s' % photolog.template_folder)
--- photolog/runserver.py
+++
@@ -1,8 +0,0 @@
-from photolog import create_app
-
-application = create_app()
-
-if __name__ == '__main__':
-    print("starting test server...")
-
-    application.run(host='0.0.0.0', port=5000, debug=True)
